316e820577b8aaef699291920caf7f3d4c822d03
Jairopian <jairopcruz@outlook.com>
2021-11-14 22:00:25 -0500

    test

--- app.py
+++ app.py
@@ -91,10 +91,5 @@ def index():
     	"index.html",
     )
 
-if __name__ == "__main__":
-	app.run(
-		host=os.getenv('IP', '0.0.0.0'),
-		port=int(os.getenv('PORT', 8080)),
-		debug=True
-	)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
 
